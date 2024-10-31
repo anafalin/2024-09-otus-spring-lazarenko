7aabf5a722a068989e265c571a0c61792515f74a
Anastasia Lazarenko <101412572+anafalin@users.noreply.github.com>
2024-10-31 21:20:27 +0300

    Task #3. Приложение по проведению тестирования студентов на Spring Boot с i18n
    migrated to Spring Boot, added support i18n, custom banner

---
+++ hw03/src/test/java/ru/otus/hw/service/TestServiceImplUnitTest.java
@@ -0,0 +1,62 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TestServiceImplUnitTest {
+    @InjectMocks
+    private TestServiceImpl testService;
+
+    @Mock
+    private LocalizedIOService ioService;
+
+    @Mock
+    private QuestionDao questionDao;
+
+    @Test
+    @DisplayName("""
+            executeTestFor
+            | test result contains student, rightAnswersCount is 1
+            | questions list is not empty, contains one question and entered correct number of answer""")
+    void executeTestFor_correctResult_questionsListNotEmpty() {
+        // given
+        var student = new Student("Mike", "Smith");
+        var question = new Question("What is not a collection in Java?", List.of(
+                new Answer("ArrayList", false),
+                new Answer("HashSet", false),
+                new Answer("Array", true),
+                new Answer("LinkedHashSet", false)));
+        var inputAnswer = 3;
+
+        // when
+        when(questionDao.findAll())
+                .thenReturn(List.of(question));
+
+        when(ioService.readIntForRangeWithPromptLocalized(anyInt(), anyInt(), anyString(), anyString()))
+                .thenReturn(inputAnswer);
+
+        var result = testService.executeTestFor(student);
+
+        // then
+        assertThat(result.getStudent()).isEqualTo(student);
+        assertThat(result.getStudent().firstName()).isEqualTo(student.firstName());
+        assertThat(result.getAnsweredQuestions()).contains(question);
+        assertThat(result.getRightAnswersCount()).isEqualTo(1);
+    }
+}
