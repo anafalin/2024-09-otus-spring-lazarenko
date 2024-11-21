225f1af32325cd7abfd9ad5952edb42036ed4a53
Lazarenko Anastasia <afalin.itgenius@gmail.com>
2024-11-21 11:21:32 +0300

    fixed bugs

--- hw06/src/main/java/ru/otus/hw/commands/AdministrationCommands.java
+++ hw06/src/main/java/ru/otus/hw/commands/AdministrationCommands.java
@@ -1,15 +1,12 @@
 package ru.otus.hw.commands;
 
-import lombok.RequiredArgsConstructor;
 import org.h2.tools.Console;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 import java.sql.SQLException;
 
-@RequiredArgsConstructor
 @ShellComponent
-
 public class AdministrationCommands {
 
     @ShellMethod(value = "Start H2 console", key = "start h2")
